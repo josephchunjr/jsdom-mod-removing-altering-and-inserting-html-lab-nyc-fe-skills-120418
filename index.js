51694903aed6f45363c590ec55467d2b122a552b
joseph Chun <jjunior.chun@gmail.com>
2019-01-22 04:20:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /* Enter the code to remove the main node element under this comment */
 
 /* Create your new element here and assign it to newHeader */
-const h1 = "victory";
+const newHeader = document.createElement(h1)
 main.remove()
-document.createElement(h1)
